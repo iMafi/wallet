fa7ea7c59febeba7a1a518ea8ba3e38361c6d60e
Dmitriy Osetrov <dosetrov@yahoo.com>
2016-07-19 22:11:23 +0300

    Initial commit with WAL-29 task

---
+++ js/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('wallet-monitor', ['ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'templates/index.html',
+            controller: 'IndexController'
+        }).when('/templates', {
+            templateUrl: 'templates/templates.html'
+        }).otherwise({
+            redirectTo: '/'
+        })
+    });
---
+++ js/controllers/IndexController.js
@@ -0,0 +1,5 @@
+'use strict';
+angular.module('wallet-monitor')
+    .controller('IndexController', function($scope) {
+        $scope.hello = "Very helpful message";
+    });
