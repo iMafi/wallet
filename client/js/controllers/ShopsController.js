655cee3a9103c80baf16fd136ba5645bf1c3cdd8
dvosetrov <dosetrov@yahoo.com>
2016-07-29 16:35:09 +0300

    added navbar directive for shops submenus

--- client/js/controllers/ShopsController.js
+++ client/js/controllers/ShopsController.js
@@ -1,4 +1,7 @@
 'use strict';
-angular.module('wallet-monitor').controller('ShopsController', function($scope) {
-    $scope.shops = "Hello from SHops Controller";
+angular.module('wallet-monitor').controller('ShopsController', function($scope, shopsFactory) {
+    shopsFactory.getAllShops().then(function (response) {
+        $scope.shops = response.data.items;
+        console.log($scope.shops);
+    });
 });
